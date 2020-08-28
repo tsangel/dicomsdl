393c07457d0bed1557ade4c7c42a8bcd7058390e
Kim, Tae-Sung <taesung.angel@gmail.com>
2020-08-29 02:40:30 +0900

    Fix temporarily compiling error related to pybind11/stl.h and experimental/optinoal

--- src/ext/pybind11/include/pybind11/stl.h
+++ src/ext/pybind11/include/pybind11/stl.h
@@ -23,28 +23,30 @@
 #pragma warning(disable: 4127) // warning C4127: Conditional expression is constant
 #endif
 
-#ifdef __has_include
-// std::optional (but including it in c++14 mode isn't allowed)
-#  if defined(PYBIND11_CPP17) && __has_include(<optional>)
 #    include <optional>
 #    define PYBIND11_HAS_OPTIONAL 1
-#  endif
-// std::experimental::optional (but not allowed in c++11 mode)
-#  if defined(PYBIND11_CPP14) && __has_include(<experimental/optional>)
-#    include <experimental/optional>
-#    define PYBIND11_HAS_EXP_OPTIONAL 1
-#  endif
-// std::variant
-#  if defined(PYBIND11_CPP17) && __has_include(<variant>)
-#    include <variant>
-#    define PYBIND11_HAS_VARIANT 1
-#  endif
-#elif defined(_MSC_VER) && defined(PYBIND11_CPP17)
-#  include <optional>
-#  include <variant>
-#  define PYBIND11_HAS_OPTIONAL 1
-#  define PYBIND11_HAS_VARIANT 1
-#endif
+// #ifdef __has_include
+// // std::optional (but including it in c++14 mode isn't allowed)
+// #  if defined(PYBIND11_CPP17) && __has_include(<optional>)
+// #    include <optional>
+// #    define PYBIND11_HAS_OPTIONAL 1
+// #  endif
+// // std::experimental::optional (but not allowed in c++11 mode)
+// #  if defined(PYBIND11_CPP14) && __has_include(<experimental/optional>)
+// #    include <experimental/optional>
+// #    define PYBIND11_HAS_EXP_OPTIONAL 1
+// #  endif
+// // std::variant
+// #  if defined(PYBIND11_CPP17) && __has_include(<variant>)
+// #    include <variant>
+// #    define PYBIND11_HAS_VARIANT 1
+// #  endif
+// #elif defined(_MSC_VER) && defined(PYBIND11_CPP17)
+// #  include <optional>
+// #  include <variant>
+// #  define PYBIND11_HAS_OPTIONAL 1
+// #  define PYBIND11_HAS_VARIANT 1
+// #endif
 
 NAMESPACE_BEGIN(pybind11)
 NAMESPACE_BEGIN(detail)
