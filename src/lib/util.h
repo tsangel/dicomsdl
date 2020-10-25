6828be643f3fa3a460912163c4a99c2239269999
Kim, Tae-Sung <taesung.angel@gmail.com>
2020-10-25 15:15:33 +0900

    Rewrite DataElement toNumber/fromNumber functions using templates

--- src/lib/util.h
+++ src/lib/util.h
@@ -8,7 +8,9 @@
 
 #include <stdarg.h>
 #include <stdio.h>
+
 #include <string>
+
 #include "dicom.h"
 
 #ifndef DICOMSDL_UTIL_H__
@@ -18,7 +20,7 @@ namespace dicom {
 
 #define MESSAGE_BUFFER_SIZE 256
 
-std::string sprint(const char * format, ...);
+std::string sprint(const char *format, ...);
 
 void swap2(uint8_t *p, size_t size);
 void swap4(uint8_t *p, size_t size);
@@ -31,6 +33,6 @@ void copyswap8(uint8_t *dst, uint8_t *src, size_t size);
 /// Return number of '\' character in data
 int count_delimiters(const uint8_t *p, const size_t size);
 
-}
+}  // namespace dicom
 
-#endif // DICOMSDL_UTIL_H__
+#endif  // DICOMSDL_UTIL_H__
