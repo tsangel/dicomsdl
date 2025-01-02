d7f4ceaac4574f85c41c98a8c033dc80824466ae
Kim, Tae-Sung <taesung.angel@gmail.com>
2025-01-03 00:10:54 +0900

    Add support python 3.13

--- setup.py
+++ setup.py
@@ -108,6 +108,8 @@ Programming Language :: Python :: 3.8
 Programming Language :: Python :: 3.9
 Programming Language :: Python :: 3.10
 Programming Language :: Python :: 3.11
+Programming Language :: Python :: 3.12
+Programming Language :: Python :: 3.13
 Topic :: Scientific/Engineering
 Topic :: Scientific/Engineering :: Medical Science Apps.
 """.strip().splitlines()
--- src/include/dicomcfg.h
+++ src/include/dicomcfg.h
@@ -32,11 +32,11 @@ typedef double float64_t;
 
 namespace dicom {
 
-const char *const DICOMSDL_VERSION = "0.109.3";
+const char *const DICOMSDL_VERSION = "0.109.4";
 
 const char *const DICOMSDL_UIDPREFIX = "1.3.6.1.4.1.56559";
 const char *const DICOMSDL_IMPLCLASSUID = "1.3.6.1.4.1.56559.1";
-const char *const DICOMSDL_IMPLVERNAME = "DICOMSDL 2022OCT";
+const char *const DICOMSDL_IMPLVERNAME = "DICOMSDL 2024DEC";
 const char *const DICOMSDL_FILESETID = "DICOMDIR";
 const char *const DICOMSDL_SOURCEAETITLE = "DICOMSDL";
 
